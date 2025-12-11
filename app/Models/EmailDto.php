0c631fddc4c0b72791623ee98d1083d2f6a7b2b4
Senechal jf <jfsenechal@gmail.com>
2025-12-11 11:02:28 +0100

    cleanup

--- app/Models/EmailDto.php
+++ app/Models/EmailDto.php
@@ -7,51 +7,87 @@ use LdapRecord\Models\Model;
 class EmailDto
 {
     public ?string $dn;
+
     public ?string $sn;
+
     public ?string $uid;
+
     public ?string $cn;
+
     public ?string $givenName;
+
     public ?string $postalAddress;
+
     public ?string $postalCode;
+
     public ?string $l;
+
     public ?string $mail;
+
     public ?string $userPassword;
+
     public ?string $employeeNumber;
+
     public int $gosaMailQuota;
+
     public ?string $gosaMailForwardingAddress;
+
     public ?string $gosaMailAlternateAddress;
+
     public ?string $uidNumber;
+
     public ?string $gidNumber;
+
     public ?string $description;
+
     public array $alias = [];
+
     public string $alias_string = '';
 
     /**
      * Staff
      */
     public ?string $sAMAccountName;
+
     public ?string $extensionMail;
+
     public ?string $title;
+
     public ?string $info;
+
     public ?string $company;
+
     public ?string $mobile;
+
     public ?string $facsimileTelephoneNumber;
+
     public ?string $homePhone;
+
     public ?string $ipPhone;
+
     public ?string $telephoneNumber;
+
     public ?string $postOfficeBox;
+
     public ?string $streetAddress;
+
     public ?string $wWWHomePage;
+
     public ?string $initials;
+
     public ?string $homeDirectory;
+
     public ?string $homeDrive;
+
     public ?string $co;
+
     public array $proxyAddresses = [];
+
     public bool $force = false;
 
     public static function loadFromModel(Model $model)
     {
-        $dto = new self();
+        $dto = new self;
         $dto->dn = $model->getDn();
         $dto->cn = $model->getFirstAttribute('cn');
         $dto->uid = $model->getFirstAttribute('uid');
@@ -63,7 +99,7 @@ class EmailDto
         $dto->l = $model->getFirstAttribute('l');
         $dto->userPassword = $model->getFirstAttribute('userPassword');
         $dto->employeeNumber = $model->getFirstAttribute('employeeNumber');
-        $dto->gosaMailQuota = $model->getFirstAttribute('gosaMailQuota', 0);
+        $dto->gosaMailQuota = $model->getFirstAttribute('gosaMailQuota', 250);
         $dto->gosaMailForwardingAddress = $model->getFirstAttribute('gosaMailForwardingAddress');
         $dto->gosaMailAlternateAddress = $model->getFirstAttribute('gosaMailAlternateAddress');
         $dto->uidNumber = $model->getFirstAttribute('uidNumber');
@@ -89,7 +125,6 @@ class EmailDto
         $dto->homeDrive = $model->getFirstAttribute('homeDrive');
         $dto->co = $model->getFirstAttribute('co');
         $dto->proxyAddresses = $model->getAttribute('proxyAddresses', []);
-
 
         return $dto;
     }
